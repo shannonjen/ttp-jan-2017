425b077cac7b44be604250cb1fd484b5e4b54a2d
Jennifer Shannon <jen.shannon@gmail.com>
2017-01-09 10:51:46 -0500

    add class ex

---
+++ day5/domform/script.js
@@ -0,0 +1,9 @@
+el1 = document.getElementById('username')
+el2 = document.getElementsByClassName('amazing')
+el3 = document.querySelector('h1')
+el4 = document.querySelectorAll('.amazing')
+
+console.log(el1);
+console.log(el2);
+console.log(el3);
+console.log(el4);
