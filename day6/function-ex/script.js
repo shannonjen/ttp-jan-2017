6dcf50be20faca06c493bc2dc191a9a316569ee4
Jennifer Shannon <jen.shannon@gmail.com>
2017-01-10 11:11:28 -0500

    Add function ex example

---
+++ day6/function-ex/script.js
@@ -0,0 +1,11 @@
+var size = area(3, 4)
+
+//function declaration
+// function area(width, height) {
+//   console.log(width * height);
+// }
+
+//function expression
+// var area = function(width, height) {
+//   console.log(width * height);
+// }
