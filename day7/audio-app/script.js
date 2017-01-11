8f847681d51843ee6af1a7263cc95aa812082778
Jennifer Shannon <jen.shannon@gmail.com>
2017-01-11 14:25:19 -0500

    Add Jukebox constructor

--- day7/audio-app/script.js
+++ day7/audio-app/script.js
@@ -1,8 +1,62 @@
+// document.getElementById('chorus').addEventListener("click", function(){
+//
+// })
 
+var myPlaylist = ["chorus.mp3","voyager.mp3"]
 
+function Jukebox(playlist){
+  this.playlist = playlist;
+  this.nowPlaying = this.playlist[0];
+  this.addTrack = function(track){
+    this.playlist.push(track)
+  }
+}
 
-var track1 = document.getElementById('chorusTrack');
+var myJukebox = new Jukebox(myPlaylist);
+myJukebox.addTrack("song3.mp3");
+console.log(myJukebox.playlist);
 
 document.getElementById("playButton").addEventListener("click", function(){
-  track1.play();
+  document.getElementById('trackPlayer').play()
 })
+
+
+
+
+var tracks = document.querySelectorAll('li');
+for (var i = 0; i < tracks.length; i++) {
+  tracks[i].addEventListener("click", function() {
+    var track = this.getAttribute("data-song-url")
+    document.getElementById('text').innerText = "Loaded: " + track
+    document.getElementById('trackPlayer').setAttribute("src", track)
+    document.getElementById("playButton").addEventListener("click", function(){
+      document.getElementById('trackPlayer').play()
+      console.log("Now Playing: "+ track)
+    })
+
+  });
+}
+// document.querySelectorAll('li').addEventListener("click", function(){
+//   var track = this.getAttribute("data-song-url")
+//   document.getElementById('text').innerText = "Loaded: " + track
+//   document.getElementById('trackPlayer').setAttribute("src", track)
+//   document.getElementById("playButton").addEventListener("click", function(){
+//     document.getElementById('trackPlayer').play()
+//     alert("Now Playing: "+ track)
+//   })
+// })
+
+// $text = $('#text')
+// $trackPlayer = $('#trackPlayer')
+// $playButton = $('playButon')
+//
+// $('li').on("click", function(){
+//   var track = this.attr("data-song-url")
+// })
+
+//
+// var track1 = document.getElementById('chorusTrack');
+//
+// document.getElementById("playButton").addEventListener("click", function(){
+//   track1.play();
+// })
