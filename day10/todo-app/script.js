72b10168ea85b5ffb8fd428fbeed4674d998d8e0
Jennifer Shannon <jen.shannon@gmail.com>
2017-01-16 21:18:57 -0500

    Add ToDo

---
+++ day10/todo-app/script.js
@@ -0,0 +1,18 @@
+$(function() {
+  var $list, $addToDoForm
+  $list = $('ul');
+  $addToDoForm = $('#addToDoForm');
+
+  $addToDoForm.on('submit', function(e) {
+    e.preventDefault();
+    var text = $('input:text').val();
+    $list.append('<li>' + text + '</li>');
+    $('input:text').val('');
+  });
+
+  $list.on('click', 'li', function() {
+    var $this = $(this);
+    console.log($this)
+    $this[0].style.textDecoration = "line-through";
+  })
+});
