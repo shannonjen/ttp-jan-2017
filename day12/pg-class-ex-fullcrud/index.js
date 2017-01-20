742f2803e12ad94f7113abe634428e2f146a8328
Jennifer Shannon <jen.shannon@gmail.com>
2017-01-20 10:51:50 -0500

    Add hats and users class ex

--- day12/pg-class-ex-fullcrud/index.js
+++ day12/pg-class-ex-fullcrud/index.js
@@ -13,6 +13,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs')
 app.set('views', './views')
+
+console.log("Check out my environment var: " + process.env.MY_ENV_VAR);
+var databaseURL = process.env.DATABASE_URL
 //get all hats
 app.get('/hats', function(req, res){
   pg.connect('postgres://localhost:5432/ttp_jan', function(err, client, done){
@@ -20,19 +23,39 @@ app.get('/hats', function(req, res){
       res.render('hats', { data: result.rows});
       console.log(result.rows)
       done();
-      pg.end();
+      // pg.end();
+    })
+  })
+})
+
+//get a specific hat
+app.get('/users/:user_id/hats/:hat_id', function(req, res){
+  pg.connect(databaseURL, function(err, client, done){
+    client.query(`select * from hats where id='${req.params.hat_id}'`, function(err, result) {
+      done();
+      if (err) {
+        console.log(err);
+        response.send("Error" + err);
+      } else {
+        res.render('hatshow', { hat: result.rows[0]});
+        console.log(result.rows)
+      }
     })
   })
 })
 
 //get all users READ
 app.get('/users', function(req, res){
-  pg.connect('postgres://localhost:5432/ttp_jan', function(err, client, done){
+  pg.connect(databaseURL, function(err, client, done){
     client.query('select * from users', function(err, result) {
-      res.render('users', { data: result.rows});
-      console.log(result.rows)
       done();
-      pg.end();
+      if (err) {
+        console.log(err);
+        response.send("Error" + err);
+      } else {
+        res.render('users', { data: result.rows});
+        console.log(result.rows)
+      }
     })
   })
 })
@@ -43,13 +66,32 @@ app.get('/users/:id', function(req,res){
     var user_id = req.params.id
     console.log(user_id)
     client.query(`select * from users where id='${user_id}'`, function(err,result){
-      res.render('show', { user: result.rows[0]})
-      console.log(result.rows);
+      var user = result.rows[0]
+      client.query(`select * from hats where user_id='${user_id}'`, function(err,result){
+        var hats = result.rows;
+        res.render('show', { user, hats})
+        done();
+      })
+
+    })
+  })
+})
+
+//create a hat for a user
+app.post('/users/:user_id/hats/:hat_id', function(req,res){
+  pg.connect('postgres://localhost:5432/ttp_jan', function(err, client, done){
+    client.query(`insert into users(name,email) values('${req.body.name}','${req.body.email}')`, function(err, result){
+      console.log(err);
+
+      res.redirect('/users');
       done();
       pg.end();
     })
   })
+
 })
+
+
 //create a user
 app.post('/users', function(req,res){
   pg.connect('postgres://localhost:5432/ttp_jan', function(err, client, done){
@@ -86,7 +128,7 @@ app.get('/delete/users/:id', function(req,res){
 app.put('/users/:id', function(req,res){
   pg.connect('postgres://localhost:5432/ttp_jan', function(err, client, done){
     var user_id = req.params.id;
-    client.query(`update from users where id='${user_id}'`, function(err, result){
+    client.query(`update users set name='${req.body.name}', email='${req.body.email}' where id='${user_id}'`, function(err, result){
   console.log(err);
       console.log(user_id);
       res.redirect('/users');
